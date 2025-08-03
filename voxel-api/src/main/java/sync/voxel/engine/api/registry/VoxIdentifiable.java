097ab0ec8200a7f5e280895a491ea0d69517e1e0
LeyCM <cm.ley.cm@gmail.com>
2025-08-03 21:56:12 +0200

    VoxRegistry with VoxIdentifiable

---
+++ voxel-api/src/main/java/sync/voxel/engine/api/registry/VoxIdentifiable.java
@@ -0,0 +1,20 @@
+/**
+ * VOXEL-LICENSE NOTICE
+ * <br><br>
+ * This software is part of VoxelSync under the Voxel Public License. <br>
+ * Source at: <a href="https://github.com/voxelsync/voxel/blob/main/LICENSE">GITHUB</a>
+ * <br><br>
+ * Copyright (c) Ley <cm.ley.cm@gmail.com> <br>
+ * Copyright (c) contributors
+ */
+package sync.voxel.engine.api.registry;
+/**
+ * Something that has a {@link VoxIdentifier}.
+ */
+public interface VoxIdentifiable {
+
+    /**
+     * Get the {@link VoxIdentifier} of the object.
+     */
+    VoxIdentifier identifier();
+}
